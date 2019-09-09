c1a87e1489b65866eaec73ee23aa4bddb73eefa0
Steven Dalton <sdalton@nvidia.com>
2019-09-09 13:48:45 -0400

    Reduced redundant code in PG-like examples

--- examples/ppo/train.py
+++ examples/ppo/train.py
@@ -1,9 +1,4 @@
-import csv
-import json
 import math
-import os
-import socket
-import sys
 import time
 import torch
 import torch.cuda.nvtx as nvtx
@@ -13,22 +8,15 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data
 
-from datetime import datetime
 from tqdm import tqdm
-from torchcule.atari import Env as AtariEnv
-from utils.openai.envs import create_vectorize_atari_env
-from utils.runtime import cuda_device_str
+from utils.initializers import args_initialize, env_initialize, log_initialize, model_initialize
 
+from a2c.helper import callback, format_time, gen_data
 from a2c.model import ActorCritic
-from a2c.helper import callback, evaluate
+from a2c.test import test
 
 try:
-    import apex
-    from apex.amp import __version__
-    from apex.parallel import DistributedDataParallel as DDP
-    from apex.fp16_utils import *
-    from apex import amp, optimizers
-    from apex.multi_tensor_apply import multi_tensor_applier
+    from apex import amp
 except ImportError:
     raise ImportError('Please install apex from https://www.github.com/nvidia/apex to run this example.')
 
@@ -59,132 +47,12 @@ class data_prefetcher():
         return states, actions, action_log_probs, returns, advantages
 
 def worker(gpu, ngpus_per_node, args):
-    args.gpu = gpu
-
-    if (args.num_ales % args.world_size) != 0:
-        raise ValueError('The num_ales({}) should be evenly divisible by the world_size({})'.format(args.num_ales, args.world_size))
-    args.num_ales = int(args.num_ales / args.world_size)
-
-    if (args.batch_size % args.world_size) != 0:
-        raise ValueError('The batch_size({}) should be evenly divisible by the world_size({})'.format(args.batch_size, args.world_size))
-    args.batch_size = int(args.num_ales / args.world_size)
-
-    num_frames_per_iter = args.num_ales * args.num_steps
-    args.num_minibatches = num_frames_per_iter / args.batch_size
-    total_steps = math.ceil(args.t_max / (args.world_size * num_frames_per_iter))
-
-    if args.distributed:
-        args.seed += args.gpu
-        torch.cuda.set_device(args.gpu)
-
-        args.rank = int(os.environ['RANK']) if 'RANK' in os.environ else 0
-        if args.multiprocessing_distributed:
-            args.rank = args.rank * ngpus_per_node + args.gpu
-
-        torch.distributed.init_process_group(backend='nccl', init_method='tcp://127.0.0.1:8632',
-                                             world_size=args.world_size, rank=args.rank)
-    else:
-        args.rank = 0
-
-    if args.lr_scale:
-        scaled_lr = args.lr * math.sqrt((args.num_ales * args.world_size) / 16)
-        if args.rank == 0:
-            print('Scaled learning rate from {:4.4f} to {:4.4f}'.format(args.lr, scaled_lr))
-        args.lr = scaled_lr
-
-    args.use_cuda_env = args.use_cuda_env and torch.cuda.is_available()
-    args.no_cuda_train = (not args.no_cuda_train) and torch.cuda.is_available()
-    args.verbose = args.verbose and (args.rank == 0)
-
-    env_device = torch.device('cuda', args.gpu) if args.use_cuda_env else torch.device('cpu')
-    train_device = torch.device('cuda', args.gpu) if (args.no_cuda_train == False) else torch.device('cpu')
-
-    np.random.seed(args.seed)
-    torch.manual_seed(np.random.randint(1, 10000))
-    if args.use_cuda_env or (args.no_cuda_train == False):
-        torch.cuda.manual_seed(np.random.randint(1, 10000))
-
-    if args.rank == 0:
-        if args.output_filename:
-            train_csv_file = open(args.output_filename, 'w', newline='')
-            train_csv_file.write(json.dumps(vars(args)))
-            train_csv_file.write('\n')
-            train_csv_writer = csv.writer(train_csv_file, delimiter=',')
-            train_csv_writer.writerow(['frames','fps','total_time',
-                                       'rmean','rmedian','rmin','rmax',
-                                       'lmean','lmedian','lmin','lmax',
-                                       'entropy','value_loss','policy_loss'])
-
-            eval_output_filename = '.'.join([''.join(args.output_filename.split('.')[:-1] + ['_test']), 'csv'])
-            eval_csv_file = open(eval_output_filename, 'w', newline='')
-            eval_csv_file.write(json.dumps(vars(args)))
-            eval_csv_file.write('\n')
-            eval_csv_writer = csv.writer(eval_csv_file, delimiter=',')
-            eval_csv_writer.writerow(['frames','total_time',
-                                       'rmean','rmedian','rmin','rmax','rstd',
-                                       'lmean','lmedian','lmin','lmax','lstd'])
-        else:
-            train_csv_file, train_csv_writer = None, None
-            eval_csv_file, eval_csv_writer = None, None
-
-        if args.plot:
-            from tensorboardX import SummaryWriter
-            current_time = datetime.now().strftime('%b%d_%H-%M-%S')
-            log_dir = os.path.join(args.log_dir, current_time + '_' + socket.gethostname())
-            writer = SummaryWriter(log_dir=log_dir)
-            for k, v in vars(args).items():
-                writer.add_text(k, str(v))
-
-        print()
-        print('PyTorch  : {}'.format(torch.__version__))
-        print('CUDA     : {}'.format(torch.backends.cudnn.m.cuda))
-        print('CUDNN    : {}'.format(torch.backends.cudnn.version()))
-        print('APEX     : {}'.format('.'.join([str(i) for i in apex.amp.__version__.VERSION])))
-        print()
-
-    if train_device.type == 'cuda':
-        print(cuda_device_str(train_device.index), flush=True)
-
-    if args.use_openai:
-        train_env = create_vectorize_atari_env(args.env_name, args.seed, args.num_ales,
-                                               episode_life=args.episodic_life, clip_rewards=False,
-                                               max_frames=args.max_episode_length)
-        observation = torch.from_numpy(train_env.reset()).squeeze(1)
-
-        test_env = create_vectorize_atari_env(args.env_name, args.seed, args.evaluation_episodes,
-                                              episode_life=False, clip_rewards=False)
-        test_env.reset()
-    else:
-        train_env = AtariEnv(args.env_name, args.num_ales, color_mode='gray', repeat_prob=0.0,
-                             device=env_device, rescale=True, episodic_life=args.episodic_life, clip_rewards=False)
-        train_env.train()
-        observation = train_env.reset(initial_steps=args.ale_start_steps, verbose=args.verbose).squeeze(-1)
-
-        test_env = AtariEnv(args.env_name, args.evaluation_episodes, color_mode='gray', repeat_prob=0.0,
-                            device='cpu', rescale=True, episodic_life=False, clip_rewards=False, frameskip=4)
+    env_device, train_device = args_initialize(gpu, ngpus_per_node, args)
+    train_env, test_env, observation = env_initialize(args, env_device)
+    train_csv_file, train_csv_writer, eval_csv_file, eval_csv_writer, summary_writer = log_initialize(args, train_device)
 
     model = ActorCritic(args.num_stack, train_env.action_space, normalize=args.normalize, name=args.env_name)
-    model = model.to(train_device).train()
-
-    if args.rank == 0:
-        print(model)
-        args.model_name = model.name
-
-    if args.use_adam:
-        optimizer = optim.Adam(model.parameters(), lr=args.lr, amsgrad=True)
-    else:
-        optimizer = optim.RMSprop(model.parameters(), lr=args.lr, eps=args.eps, alpha=args.alpha)
-
-    decay = 1.0 / total_steps
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.ppo_epoch, gamma=1.0 - decay)
-
-    model, optimizer = amp.initialize(model, optimizer,
-                                      opt_level=args.opt_level,
-                                      loss_scale=args.loss_scale
-                                     )
-
-    if args.distributed:
-        model = DDP(model, delay_allreduce=True)
+    model, optimizer = model_initialize(args, model, train_device)
 
     shape = (args.num_steps + 1, args.num_ales, args.num_stack, *train_env.observation_space.shape[-2:])
     states = torch.zeros(shape, device=train_device, dtype=torch.float32)
@@ -211,7 +79,12 @@ def worker(gpu, ngpus_per_node, args):
 
     maybe_npy = lambda a: a.numpy() if args.use_openai else a
 
-    torch.cuda.synchronize()
+    num_frames_per_iter = args.num_ales * args.num_steps
+    args.num_minibatches = num_frames_per_iter / args.batch_size
+    total_steps = math.ceil(args.t_max / (args.world_size * num_frames_per_iter))
+
+    decay = 1.0 / total_steps
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.ppo_epoch, gamma=1.0 - decay)
 
     iterator = range(total_steps)
     if args.rank == 0:
@@ -221,16 +94,28 @@ def worker(gpu, ngpus_per_node, args):
 
     train_stream = torch.cuda.Stream()
 
+    torch.cuda.synchronize()
+
     for update in iterator:
 
         T = args.world_size * update * num_frames_per_iter
         if (args.rank == 0) and (T >= evaluation_offset):
             evaluation_offset += args.evaluation_interval
-            eval_lengths, eval_rewards = evaluate(args, T, total_time, model, test_env, eval_csv_writer, eval_csv_file)
+            eval_lengths, eval_rewards = test(args, model, test_env)
+
+            lmean, lmedian, lmin, lmax, lstd = gen_data(eval_lengths)
+            rmean, rmedian, rmin, rmax, rstd = gen_data(eval_rewards)
+            length_data = '(length) min/max/mean/median: {lmin:4.1f}/{lmax:4.1f}/{lmean:4.1f}/{lmedian:4.1f}'.format(lmin=lmin, lmax=lmax, lmean=lmean, lmedian=lmedian)
+            reward_data = '(reward) min/max/mean/median: {rmin:4.1f}/{rmax:4.1f}/{rmean:4.1f}/{rmedian:4.1f}'.format(rmin=rmin, rmax=rmax, rmean=rmean, rmedian=rmedian)
+            print('[training time: {}] {}'.format(format_time(total_time), ' --- '.join([length_data, reward_data])))
+
+            if eval_csv_writer and eval_csv_file:
+                eval_csv_writer.writerow([T, total_time, rmean, rmedian, rmin, rmax, rstd, lmean, lmedian, lmin, lmax, lstd])
+                eval_csv_file.flush()
 
             if args.plot:
-                writer.add_scalar('eval/rewards_mean', eval_rewards.mean().item(), T, walltime=total_time)
-                writer.add_scalar('eval/lengths_mean', eval_lengths.mean().item(), T, walltime=total_time)
+                summary_writer.add_scalar('eval/rewards_mean', rmean, T, walltime=total_time)
+                summary_writer.add_scalar('eval/lengths_mean', lmean, T, walltime=total_time)
 
         start_time = time.time()
 
@@ -387,4 +272,5 @@ def worker(gpu, ngpus_per_node, args):
 
     if args.use_openai:
         train_env.close()
+    if args.use_openai_test_env:
         test_env.close()
